785eff666291f10c07b5fc61d6e35919c955579b
RustyNova <rusty.nova.jsb@gmail.com>
2023-04-20 11:15:25 +0200

    Resolved Merge Conflict

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
 export {Option, OptionInterface, Some, None} from "./option/option";
-export {Result,ResultInterface, Err, Ok} from "./result/result";
+export {Result, ResultInterface, Err, Ok} from "./result/result";
